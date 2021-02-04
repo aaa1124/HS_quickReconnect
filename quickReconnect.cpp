bedcaa83b0e56960fc31d6ffeea15e9432b93f75
aaa1124 <captain.falcon1124@gmail.com>
2021-02-04 15:45:02 +0800

    v0.3

--- quickReconnect.cpp
+++ quickReconnect.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc/malloc.h>
 #include <cstdlib>
 
 #include <iostream>
@@ -10,6 +9,7 @@
 #include <fstream>
 #include <unistd.h>
 #include <sstream>  
+#include <filesystem>
 using namespace std;
 
 
@@ -79,6 +79,104 @@ struct checkPoint{
 		}
 	}
 };
+
+
+
+class HS_log{
+		ifstream *file;
+
+		static inline string max_func(vector<string>::iterator lst_beg, int size){
+			if(size <= 0) return "";
+			if(size == 1){
+				return *lst_beg;
+
+			}else if(size == 2){
+				return max<string>(*lst_beg, *(lst_beg+1));
+			}
+			return max(max(lst_beg[0], lst_beg[1]), max_func(lst_beg+2, size - 2));
+		}
+
+		static inline string pathAppend(const string& p1, const string& p2) {
+
+			char sep = '/';
+			string tmp = p1;
+
+			
+
+			if (p1[p1.length() - 1] != sep) { 
+				tmp += sep;                
+				return(tmp + p2);
+			}else
+				return(p1 + p2);
+		}
+
+
+	public:
+		~HS_log(){
+			file->close();
+			delete file;
+		}
+		HS_log(const string PATH ){
+			file = new ifstream(PATH);
+			string line;
+			while(getline(*file, line));
+			//file->clear();
+		}
+
+		int check_is_disconnected(){
+			string line;
+			int  cg, lg;
+
+			file->clear();
+
+			cg = file->tellg();
+			file->seekg(0, file->end);
+			lg = file->tellg();
+			file->seekg(cg);
+			if(cg < lg){
+
+				
+				regex cd_regex("\\[BattleNet\\] \\[ReceiveCallback\\] ClientConnection.ReceiveCallback called after the connection has been disconnected");
+				regex cd2_regex("Network.GotoGameServer.+reconnecting=True");
+				smatch m;
+				while(getline(*file, line)){
+
+					if (regex_search(line , m , cd_regex) || regex_search(line , m , cd2_regex)){
+						return 1;
+					}
+
+				}
+			}
+			return 0;
+		}
+		int is_open(){
+			return file->is_open();
+		}
+
+
+		static string get_hslog_fn(const string PATH){
+			regex hs_log_filename_regex("hearthstone_[0-9]{4}_[0-9]{2}_[0-9]{2}.+\\.log");
+			smatch m;
+			string s;
+			vector<string> lst;
+
+			for (const auto  &entry : filesystem::directory_iterator(PATH)){
+				s = entry.path().filename().generic_string();
+				if(regex_search(s, m , hs_log_filename_regex)){
+					lst.push_back(s);
+				}
+			}
+			if(lst.size() > 0) {
+				string result(HS_log::max_func(lst.begin(), lst.size()));
+				result = HS_log::pathAppend(PATH, result);
+				return result;
+			}else{
+				return "";
+			}
+
+		}
+};
+
 
 
 void read_current_rules(string &rules){
@@ -160,20 +258,29 @@ void append_rules(string &rules, vector<string> &ip_list){
 
 int main(int argc, char** argv){
 
-	checkPoint cp;
 
+	checkPoint cp;
 	if (!cp.is_running_with_sudo){
 		printf("please run with sudo\n");
 		
 		return 0;
 	}
-
-	int sleep_time = 6;
-	if(argc > 1) sleep_time = atoi(argv[1]);
-	printf("wait for %d seconds to reconnect. \n" , sleep_time);
-
-
+	
+	int auto_dc;
+	int sleep_time;
 	vector<string> ip_list;
+	string rules;
+
+
+	auto_dc = 1;
+	if(argc > 1) {
+		sleep_time = atoi(argv[1]);
+		printf("wait for %d seconds to reconnect. \n" , sleep_time);
+		auto_dc = 0;
+	}
+
+
+	
 	if(!capture_hs_ip(ip_list)){
 		printf("Hearthstone is not running. \n");
 		return 0;
@@ -187,17 +294,40 @@ int main(int argc, char** argv){
 	append_rules(new_rules, ip_list);
 	*/
 
-	string test_rules;
-	append_rules(test_rules, ip_list);
+
+	append_rules(rules, ip_list);
 
 	ostringstream command;
-	command << "printf \"" << test_rules << "\" | cat /etc/pf.conf -  | pfctl -ef -  &>/dev/null";
-	system(command.str().c_str());
+	command << "printf \"" << rules << "\" | cat /etc/pf.conf -  | pfctl -ef -  &>/dev/null";
+	
 
 
 
 	printf("disconnecting............\n");
-	sleep(sleep_time);
+	if(auto_dc){
+		string HSLOG_PATH = HS_log::get_hslog_fn("/Applications/Hearthstone/Logs");
+		HS_log log(HSLOG_PATH);
+
+
+		system(command.str().c_str());
+		int count = 0;
+		while (!log.check_is_disconnected()){
+			usleep(200000);
+			if (count>200){
+				printf("something went wrong...\n");
+				
+				goto end; 
+			}
+			count++;
+
+			
+		}
+
+	}else{
+		system(command.str().c_str());
+		sleep(sleep_time);
+	}
+	
 	printf(".............reconnecting\n");
 
 	/*
@@ -205,11 +335,8 @@ int main(int argc, char** argv){
 	command << "printf \"" << ori_rules << "\" | pfctl -ef -   &>/dev/null";
 	system(command.str().c_str());
 	*/
-	system("pfctl -f /etc/pf.conf &>/dev/null");
-	if (!cp.is_enabled) system("pfctl -d");
-
-
-
-
-	return 0;
+	end:
+		system("pfctl -f /etc/pf.conf &>/dev/null");
+		if (!cp.is_enabled) system("pfctl -d &>/dev/null");
+		return 0;
 }
